ed319cc9c3733272e5a1ed3172803d71be2a5747
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2019-05-31 01:32:38 +0800

    no message

--- Julia/basic.jl
+++ Julia/basic.jl
@@ -1,11 +1,25 @@
-using Roots
-function polynomialGenerator(a...)
-    n = length(a)-1
-    poly = function(x)
-        return sum([a[i+1]*x^i for i in 0:n])
-    end
-    return poly
+using LinearAlgebra, StatsBase
+# Transition probability matrix
+P = [0.5 0.4 0.1;
+     0.3 0.2 0.5;
+     0.5 0.3 0.2]
+# First way
+piProb1 = (P^100)[1,:]
+# Second way
+A = vcat((P' - I)[1:2,:],ones(3)')
+b = [0 0 1]'
+piProb2 = A\b
+# Third way
+eigVecs = eigvecs(copy(P'))
+highestVec = eigVecs[:,findmax(abs.(eigvals(P)))[2]]
+piProb3 = Array{Float64}(highestVec)/norm(highestVec,1);
+# Fourth way
+numInState = zeros(3)
+state = 1
+N = 10^6
+for t in 1:N
+    numInState[state] += 1
+    global state = sample(1:3,weights(P[state,:]))
 end
-polynomial = polynomialGenerator(1,3,-10)
-zeroVals = find_zeros(polynomial,-10,10)
-println("Zeros of the function f(x): ", zeroVals)
+piProb4 = numInState/N
+[piProb1 piProb2 piProb3 piProb4]
